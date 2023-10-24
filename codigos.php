a01c7939b834e890f461f8c2516e15ae76dd3181
eduardobarros <eduzial@gmail.com>
2023-10-24 10:37:55 -0300

    Add files via upload

---
+++ codigos.php
@@ -0,0 +1,13 @@
+    <div class="ancora" id="codigos-de-conduta"></div>
+    <section class="codigos">
+      <div class="codigos-textos">
+        <h2>Códigos de Conduta</h2>
+        <p class="codigos-intro">O Programa de compliance da Dasa é composto por três códigos de conduta. Aqui na Dasa, temos a preocupação em separá-los com o objetivo de dar foco aos diferentes públicos a quem eles se destinam.</p>
+        <p class="codigos-conheca"><br><b>Clique para conhecer nossos Códigos de Conduta:</b></p>
+      </div>
+      <div class="codigos-botoes">
+        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112322/Dasa-C%C3%B3digo-de-Conduta-dos-M%C3%A9dicos_V3.pdf" title="Códido de Conduta dos Médicos" target="_blank">Código de Conduta dos Médicos <img src="assets/img/codigo-01.svg" alt="Código de Conduta dos Médicos"></a>
+        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112738/C%C3%B3digo-de-Conduta-dos-Colaboradores_V2_Bits.pdf" title="Códido de Conduta dos Colaboradores" target="_blank">Código de Conduta dos Colaboradores <img src="assets/img/codigo-02.svg" alt="Código de Conduta dos Colaboradores"></a>
+        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112727/C%C3%B3digo-de-Conduta-de-Terceiros_V2_Bits.pdf" title="Códido de Conduta de Terceiros" target="_blank">Código de Conduta de Terceiros <img src="assets/img/codigo-03.svg" alt="Código de Conduta de Terceiros"></a>
+      </div>
+    </section>
