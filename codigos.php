d2b20b584a8ddc41e38c95a5f7a6a1f02231829c
eduardobarros <eduzial@gmail.com>
2023-10-24 10:50:29 -0300

    Add files via upload

--- codigos.php
+++ codigos.php
@@ -6,8 +6,8 @@
         <p class="codigos-conheca"><br><b>Clique para conhecer nossos Códigos de Conduta:</b></p>
       </div>
       <div class="codigos-botoes">
-        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112322/Dasa-C%C3%B3digo-de-Conduta-dos-M%C3%A9dicos_V3.pdf" title="Códido de Conduta dos Médicos" target="_blank">Código de Conduta dos Médicos <img src="assets/img/codigo-01.svg" alt="Código de Conduta dos Médicos"></a>
-        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112738/C%C3%B3digo-de-Conduta-dos-Colaboradores_V2_Bits.pdf" title="Códido de Conduta dos Colaboradores" target="_blank">Código de Conduta dos Colaboradores <img src="assets/img/codigo-02.svg" alt="Código de Conduta dos Colaboradores"></a>
-        <a href="https://bkt-sa-east-1-cms-2-assets-prd.s3.sa-east-1.amazonaws.com/prd/2022/04/19112727/C%C3%B3digo-de-Conduta-de-Terceiros_V2_Bits.pdf" title="Códido de Conduta de Terceiros" target="_blank">Código de Conduta de Terceiros <img src="assets/img/codigo-03.svg" alt="Código de Conduta de Terceiros"></a>
+        <a href="assets/download/codigo-conduta-dasa-medicos.pdf" title="Códido de Conduta dos Médicos" target="_blank">Código de Conduta dos Médicos <img src="assets/img/codigo-01.svg" alt="Código de Conduta dos Médicos"></a>
+        <a href="assets/download/codigo-conduta-dasa-colaboradores.pdf" title="Códido de Conduta dos Colaboradores" target="_blank">Código de Conduta dos Colaboradores <img src="assets/img/codigo-02.svg" alt="Código de Conduta dos Colaboradores"></a>
+        <a href="assets/download/codigo-conduta-dasa-terceiros.pdf" title="Códido de Conduta de Terceiros" target="_blank">Código de Conduta de Terceiros <img src="assets/img/codigo-03.svg" alt="Código de Conduta de Terceiros"></a>
       </div>
     </section>
