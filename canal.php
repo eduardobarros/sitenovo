60627da56dcd524eaad50a8a3fec4fd283c94ce1
eduardobarros <eduzial@gmail.com>
2023-10-27 14:59:33 -0300

    Add files via upload

---
+++ canal.php
@@ -0,0 +1,13 @@
+    <div class="ancora" id="canal-de-conduta"></div>
+    <section class="canal">
+      <img src="assets/img/canal-foto-mobile.webp" title="Pesquisa" class="canal-foto-mobile canal-foto" loading="lazy">
+      <img src="assets/img/canal-foto-desktop.webp" title="Pesquisa" class="canal-foto-desktop canal-foto" loading="lazy">
+      <div class="canal-textos">
+        <h2>Canal de Conduta</h2>
+        <ul>
+          <li><img src="assets/img/check-vermelho.svg" alt="Check"> O Canal de Conduta é uma ferramenta sigilosa que possibilita a qualquer pessoa, interna ou externa à Companhia, realizar denúncias, de forma anônima ou não. É o local onde deve ser reportado suspeitas de comportamentos indevidos ou posturas que violem os Códigos de Conduta e as Políticas Internas ou as leis vigentes para que sejam devidamente investigadas. </li>
+          <li><img src="assets/img/check-vermelho.svg" alt="Check"> Não permitimos e nem toleramos retaliação aos colaboradores, administradores, clientes ou terceiros que realizarem denuncias, assim como os envolvidos nas investigações.</li>
+        </ul>
+        <a href="https://www.canaldaconduta.com.br/" target="_blank" title="Acessar Canal de Conduta" class="botao">Acessar Canal de Conduta</a>
+      </div>
+    </section>
