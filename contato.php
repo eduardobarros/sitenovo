14a2b6949588fa314ceef8e2c2783c5de49aee1a
eduardobarros <eduzial@gmail.com>
2023-11-06 11:38:18 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Programação - Congresso Dasa Oncologia 2023</title>
+  <meta name="description" content="Um evento totalmente voltado para o compartilhamento do conhecimento científico, pensado e organizado detalhadamente para levar aos nossos convidados o que há de mais inovador e moderno sobre os cuidados oncológicos, visando uma medicina preditiva, preventiva e cuidar sempre e por inteiro." />
+  <meta name="robots" content="noindex">
+
+  <meta property="og:title" content="Congresso Dasa Oncologia 2023" />
+  <meta property="og:site_name" content="Dasa" />
+  <meta property="og:description" content="Um evento totalmente voltado para o compartilhamento do conhecimento científico, pensado e organizado detalhadamente para levar aos nossos convidados o que há de mais inovador e moderno sobre os cuidados oncológicos, visando uma medicina preditiva, preventiva e cuidar sempre e por inteiro.">
+
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="pt-br" />
+
+  <meta property="og:image" content="https://placcopazzinidev09.com.br/assets/img/og.jpg">
+  <meta property="og:image:type" content="image/jpg">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+
+  <meta property="twitter:title" content="Congresso Dasa Oncologia 2023" />
+  <meta property="twitter:description" content="O Programa de Compliance da Dasa é uma ferramenta de todos a quem ele se aplica e consolida o conjunto de medidas institucionais para o desenvolvimento de suas atividades de forma ética e íntegra." />
+  <meta property="twitter:image" content="https://placcopazzinidev09.com.br/assets/img/og.jpg" />
+
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <link rel="shortcut icon" href="assets/img/favicon.png" />
+
+  <!-- PRELOAD -->
+  <link rel="preload" as="style" href="assets/css/main.css" >
+  <link rel="preload" as="script" href="assets/js/jquery.min.js">
+  <link rel="preload" as="script" href="assets/js/scroll-out.js">
+  <link rel="preload" as="script" href="assets/js/scripts.js">
+
+  <link rel="preload" as="image" src="assets/img/slide01.webp" >
+  <link rel="preload" as="image" src="assets/img/slide02.webp" >
+  <link rel="preload" as="image" src="assets/img/slide03.webp" >
+  <link rel="preload" as="image" src="assets/img/slide04.webp" >
+  <link rel="preload" as="image" src="assets/img/slide05.webp" >
+
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/flickity-docs.css">
+
+</head>
+<body>
+
+  <?php include 'inc/header.php' ?>
+  <content>
+
+    <div class="breadcrumb">Contato</div>
+
+    <div class="conteudo conteudo-programacao">
+      <h1>Fale conosco</h1>
+        <div class="conteudo-textos">
+
+
+            <div class="programacao-abertura-horario" style=" width: 100%; max-width: 700px; margin: auto ;">
+              <div>
+                <br><br><span style="display: block; font-family: DasaBold; border-bottom: 1px solid #9E76B4; font-size: 24px; padding-bottom: 10px; margin-bottom: 10px;">Comercial CCM Group</span>
+                Eder Cussolini<br>
+                <a href="mailto:eder.cussolini@ccmgroup.com.br" style="padding: 5px 0; margin: 0;" title="E-mail para Eder Cussolini">eder.cussolini@ccmgroup.com.br</a><br>
+                <a href="tel:011945953094" title="Ligar para Eder Cussolini" style="font-family: DasaBold, sans-serif; padding: 5px 0; margin: 0;">(+55 11) 94595-3094</a>
+              </div>
+
+              <div>
+                <br><br><span style="display: block; font-family: DasaBold; border-bottom: 1px solid #9E76B4; font-size: 24px; padding-bottom: 10px; margin-bottom: 10px;">Informações</span>
+                Paula Makarov<br>
+                <a href="mailto:paula.makarov@ccmgroup.com.br" style="padding: 5px 0; margin: 0;" title="E-mail para Paula Makarov">paula.makarov@ccmgroup.com.br</a><br>
+                <a href="tel:021991749186" title="Ligar para Paula Makarov" style="font-family: DasaBold, sans-serif; padding: 5px 0; margin: 0;">(+55 21) 99174-9186</a>
+              </div>
+
+            </div>
+
+      </div>
+    </div>
+
+    <?php include 'inc/hotel-capa.php' ?>
+  </content>
+    <?php include 'inc/footer.php' ?>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/scroll-out.js"></script>
+  <script src="assets/js/scripts.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ header.php
@@ -0,0 +1,32 @@
+  <header class="header-menu" id="header-menu">
+
+    <img src="assets/img/dasa-azul.svg" alt="Dasa Oncologia" id="header-dasa-azul">
+    <img src="assets/img/dasa-branco.svg" alt="Dasa Oncologia" id="header-dasa-branco">
+
+    <img src="assets/img/icone-menu.svg" alt="Menu" class="icone-menu">
+
+    <nav class="nav-mobile">
+      <a href="./" class="nav-link" title="Home">Home</a>
+      <a href="evento" class="nav-link" title="O evento">O evento</a>
+      <a href="comissao" class="nav-link" title="Comissão Organizadora">Comissão Organizadora</a>
+      <a href="local" class="nav-link" title="Local">Local</a>
+      <a href="programacao" class="nav-link" title="Programação">Programação</a>
+      <a href="palestrantes" class="nav-link" title="Palestrantes">Palestrantes</a>
+      <a href="patrocinadores" class="nav-link" title="Patrocinadores">Patrocinadores</a>
+      <a href="contato" class="nav-link" title="Contato">Contato</a>
+      <a href="https://dasaeducaeventos.com.br/app/congresso-dasa-oncologia-2023" target="_blank" class="nav-link" title="Inscrições">Inscrições</a>
+    </nav>
+
+    <nav class="nav-desktop">
+      <a href="./" class="nav-link-desktop" id="nav-link-desktop1" title="Home">Home</a>
+      <a href="evento" class="nav-link-desktop" id="nav-link-desktop2" title="O evento">O evento</a>
+      <a href="comissao" class="nav-link-desktop" id="nav-link-desktop3" title="Comissão Organizadora">Comissão Organizadora</a>
+      <a href="local" class="nav-link-desktop" id="nav-link-desktop4" title="Local">Local</a>
+      <a href="programacao" class="nav-link-desktop" id="nav-link-desktop5" title="Programação">Programação</a>
+      <a href="palestrantes" class="nav-link-desktop" id="nav-link-desktop6" title="Palestrantes">Palestrantes</a>
+      <a href="patrocinadores" class="nav-link-desktop" id="nav-link-desktop7" title="Patrocinadores">Patrocinadores</a>
+      <a href="contato" class="nav-link-desktop" id="nav-link-desktop8" title="Contato">Contato</a>
+      <a href="https://dasaeducaeventos.com.br/app/congresso-dasa-oncologia-2023" target="_blank" class="nav-link-desktop" id="nav-link-desktop9" title="Inscrições">Inscrições</a>
+    </nav>
+    
+  </header>
